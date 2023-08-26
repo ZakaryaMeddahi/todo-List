c4ad9f0a6c93db6c6739b1d65519cf7c0ada969d
l3alisidahmed <sidoabdelali06@gmail.com>
2023-08-26 15:15:47 +0100

    add update task

--- public/js/deleteTask.js
+++ public/js/deleteTask.js
@@ -1,5 +1,5 @@
-const deleteIcons = document.querySelectorAll(".fa-trash");
 const cardTask = document.querySelectorAll(".Card-Task");
+const deleteIcons = document.querySelectorAll(".fa-trash");
 
 deleteIcons.forEach(element => {
     element.addEventListener("click", (event) => {
@@ -7,8 +7,8 @@ deleteIcons.forEach(element => {
         console.log(target)
         for (let index = 0; index < cardTask.length; index++) {
             if (target.id == cardTask[index].getAttribute("iden")) {
-                cardTask[index].remove()
-            }  
+                cardTask[index].remove();
+            }
         }
     });
 });
